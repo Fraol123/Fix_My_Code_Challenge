affe9f798d5209ffacd5c4ebb547a02f728f9327
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-21 10:11:04 +0300

    new

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+module for Square clas
+"""
 
 
 class Square:
-    """Square class"""
+    """ Square class """
+
     def __init__(self, *args, **kwargs):
-        """Square intialization"""
+        """ initiailize a new Square obj """
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -14,16 +18,17 @@ class Square:
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ formatted attributes """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
