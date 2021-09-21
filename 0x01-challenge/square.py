03c72696efaeaa973772dffd397e776b9ebf4193
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-21 10:07:01 +0300

    works

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 
-class square():
-
-    width = 0
-    height = 0
-
+class Square:
+    """Square class"""
     def __init__(self, *args, **kwargs):
+        """Square intialization"""
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -20,9 +20,10 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
